f8fb3153af31cfbe2ea076390409f6dece09d58e
kakakaya <kakakaya@users.noreply.github.com>
2023-04-11 01:08:48 +0900

    Impl Ctrl+Enter posting (#5)

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,56 +1,108 @@
 <script>
-  import logo from './assets/images/logo-universal.png'
-  import {Chikuwa, Greet, Post} from '../wailsjs/go/main/App.js'
+    import logo from './assets/images/logo-universal.png'
+    import {
+        Chikuwa,
+        Greet,
+        Post
+    } from '../wailsjs/go/main/App.js'
+    import {
+        WindowCenter,
+        WindowHide,
+        WindowShow
+    } from '../wailsjs/runtime/runtime.js'
 
-  let resultText = "Please enter your name below 👇"
-  let text
+    let resultText = "Please enter your name below 👇"
+    let text
+    let placeholder
 
-  function greet() {
-    Greet(text).then(result => resultText = result)
-  }
+    function countGrapheme(string) {
+        const segmenter = new Intl.Segmenter("ja", {
+            granularity: "grapheme"
+        });
+        return [...segmenter.segment(string)].length;
+    }
 
- function post() {
-     Post(text).then(result => resultText = result)
- }
+    function greet() {
+        Greet(text).then(result => resultText = result)
+    }
 
- function chikuwa() {
-     Chikuwa().then(result => resultText = result)
- }
+    function post() {
+        Post(text).then(result => {
+            resultText = result
+            text = ""
+        })
+    }
 
+    function earthquake() {
+        text = "地震だ!"
+        // post();
+    }
+
+    function chikuwa() {
+        Chikuwa().then(result => {
+            resultText = result
+            text = ""
+        })
+    }
+
+    function windowHide() {
+        WindowHide()
+    }
+
+    function windowShow() {
+        WindowShow()
+    }
+
+    function handleKeyDown(event) {
+        if ((event.ctrlKey || event.metaKey) && event.key == 'a') {
+            event.preventDefault();
+            text = "全部選択"
+        } else if ((event.ctrlKey || event.metaKey) && event.key == 'Enter') {
+            event.preventDefault();
+            post();
+        } else if (event.key == 'F2') {
+            event.preventDefault();
+            // open config
+        } else if (event.key == 'F9') {
+            event.preventDefault();
+            earthquake();
+        }
+    }
+    window.addEventListener("keydown", handleKeyDown);
 </script>
 
 <main>
-    <!-- <img alt="Wails logo" id="logo" src="{logo}"> -->
-  <div class="result" id="result">{resultText}</div>
-  <div class="input-box" id="input">
-    <textarea autocomplete="off" bind:value={text} id="name" />
-    <button class="btn" on:click={greet}>Greet</button>
-    <button class="btn" on:click={post}>Post</button>
-    <button class="btn" on:click={chikuwa}>ちくわ。</button>
-  </div>
+    <div class="result" id="result">{resultText}</div>
+    <div class="input-box" id="input">
+        <textarea autocomplete="off" bind:value={text} placeholder={placeholder} id="name" />
+    </div>
+    <div class="buttons">
+        <p>
+            ↓仮のボタン群
+        </p>
+        <button class="btn" on:click={post}>Post(Ctrl+Enter)</button>
+        <button class="btn" on:click={chikuwa}>ちくわ。</button>
+        <button class="btn" on:click={WindowCenter}>画面中央</button>
+        <button class="btn" on:click={windowHide}>画面hide</button>
+        <button class="btn" on:click={windowShow}>画面show</button>
+        <button class="btn" on:click={earthquake}>地震だ！(F9)</button>
+    </div>
+    <p id="input-length"></p>
 </main>
 
 <style>
+    body {
+        font-family: "Avenir-Roman", "Arial", "游ゴシック体", YuGothic, "游ゴシック Medium", "Yu Gothic Medium", "游ゴシック", "Yu Gothic", sans-serif, ui-sans-serif, system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif, Helvetica Neue, Arial, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+    }
 
-  #logo {
-    display: block;
-    width: 50%;
-    height: 50%;
-    margin: auto;
-    padding: 10% 0 0;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: 100% 100%;
-    background-origin: content-box;
-  }
+    .result {
+        height: 1em;
+        line-height: 2em;
+        margin: 1.5rem auto;
+    }
 
-  .result {
-    height: 20px;
-    line-height: 20px;
-    margin: 1.5rem auto;
-  }
-
-  .input-box .btn {
+    /*
+    .input-box .btn {
     width: 60px;
     height: 30px;
     line-height: 30px;
@@ -59,40 +111,40 @@
     margin: 0 0 0 20px;
     padding: 0 8px;
     cursor: pointer;
-  }
+    }
 
-  .input-box .btn:hover {
+    .input-box .btn:hover {
     background-image: linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%);
     color: #333333;
-  }
+    }
+  */
+    .input-box .input {
+        border: none;
+        border-radius: 3px;
+        outline: none;
+        height: 30px;
+        line-height: 30px;
+        padding: 0 10px;
+        background-color: rgba(240, 240, 240, 0.2);
+        -webkit-font-smoothing: antialiased;
 
-  .input-box .input {
-    border: none;
-    border-radius: 3px;
-    outline: none;
-    height: 30px;
-    line-height: 30px;
-    padding: 0 10px;
-    background-color: rgba(240, 240, 240, 1);
-    -webkit-font-smoothing: antialiased;
-  }
+    }
 
-  .input-box .input:hover {
-    border: none;
-    background-color: rgba(255, 255, 255, 1);
-  }
+    .input-box .input:hover {
+        border: none;
+        background-color: rgba(255, 255, 255, 0.2);
+    }
 
-  .input-box .input:focus {
-    border: none;
-    background-color: rgba(255, 255, 255, 1);
-  }
+    .input-box .input:focus {
+        border: none;
+        background-color: rgba(255, 255, 255, 0.2);
+    }
 
-  textarea {
-    color: white;
-    background: transparent;
-    resize: none;
-    border: 0 none;
-    width: 100%;
-  }
-
+    textarea {
+        color: white;
+        background: transparent;
+        resize: none;
+        border: 0 none;
+        width: 100%;
+    }
 </style>
