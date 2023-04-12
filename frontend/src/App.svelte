e9138492038587da776ad418edefb52bf50ef3ee
kakakaya <kakakaya@users.noreply.github.com>
2023-04-13 01:36:46 +0900

    Feature/v4 (#6)
    * Changing GitHub Actions
    
    * Add release-drafter.yml
    
    * Make it draggable

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,7 +2,6 @@
     import logo from './assets/images/logo-universal.png'
     import {
         Chikuwa,
-        Greet,
         Post
     } from '../wailsjs/go/main/App.js'
     import {
@@ -11,7 +10,6 @@
         WindowShow
     } from '../wailsjs/runtime/runtime.js'
 
-    let resultText = "Please enter your name below 👇"
     let text
     let placeholder
 
@@ -22,25 +20,21 @@
         return [...segmenter.segment(string)].length;
     }
 
-    function greet() {
-        Greet(text).then(result => resultText = result)
-    }
-
     function post() {
         Post(text).then(result => {
-            resultText = result
+            placeholder = result
             text = ""
         })
     }
 
-    function earthquake() {
-        text = "地震だ!"
-        // post();
+ function earthquake() {
+     text = "地震だ!"
+     // post();
     }
 
     function chikuwa() {
         Chikuwa().then(result => {
-            resultText = result
+            placeholder = result
             text = ""
         })
     }
@@ -71,14 +65,14 @@
     window.addEventListener("keydown", handleKeyDown);
 </script>
 
-<main>
-    <div class="result" id="result">{resultText}</div>
+<main style="--wails-draggable:drag">
     <div class="input-box" id="input">
         <textarea autocomplete="off" bind:value={text} placeholder={placeholder} id="name" />
     </div>
+    <hr/>
     <div class="buttons">
         <p>
-            ↓仮のボタン群
+            ボタン群(仮)
         </p>
         <button class="btn" on:click={post}>Post(Ctrl+Enter)</button>
         <button class="btn" on:click={chikuwa}>ちくわ。</button>
@@ -91,9 +85,9 @@
 </main>
 
 <style>
-    body {
-        font-family: "Avenir-Roman", "Arial", "游ゴシック体", YuGothic, "游ゴシック Medium", "Yu Gothic Medium", "游ゴシック", "Yu Gothic", sans-serif, ui-sans-serif, system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif, Helvetica Neue, Arial, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
-    }
+ body {
+     font-family: "Avenir-Roman", "Arial", "游ゴシック体", YuGothic, "游ゴシック Medium", "Yu Gothic Medium", "游ゴシック", "Yu Gothic", sans-serif, ui-sans-serif, system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif, Helvetica Neue, Arial, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+ }
 
     .result {
         height: 1em;
@@ -101,23 +95,6 @@
         margin: 1.5rem auto;
     }
 
-    /*
-    .input-box .btn {
-    width: 60px;
-    height: 30px;
-    line-height: 30px;
-    border-radius: 3px;
-    border: none;
-    margin: 0 0 0 20px;
-    padding: 0 8px;
-    cursor: pointer;
-    }
-
-    .input-box .btn:hover {
-    background-image: linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%);
-    color: #333333;
-    }
-  */
     .input-box .input {
         border: none;
         border-radius: 3px;
@@ -125,7 +102,7 @@
         height: 30px;
         line-height: 30px;
         padding: 0 10px;
-        background-color: rgba(240, 240, 240, 0.2);
+        background-color: rgba(240, 240, 240, 0);
         -webkit-font-smoothing: antialiased;
 
     }
@@ -140,11 +117,12 @@
         background-color: rgba(255, 255, 255, 0.2);
     }
 
-    textarea {
-        color: white;
-        background: transparent;
-        resize: none;
-        border: 0 none;
-        width: 100%;
-    }
+ textarea {
+     color: white;
+     font-family: "Avenir-Roman", "Arial", "游ゴシック体", YuGothic, "游ゴシック Medium", "Yu Gothic Medium", "游ゴシック", "Yu Gothic", sans-serif, ui-sans-serif, system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif, Helvetica Neue, Arial, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+     background: transparent;
+     resize: none;
+     border: 0 none;
+     width: 100%;
+ }
 </style>
