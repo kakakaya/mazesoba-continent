91e53e5b952da338dc09146a2c4d1eaf2856f47d
kakakaya <kakakaya+git@gmail.com>
2024-03-27 00:47:09 +0900

    Writing dispatchInput

--- frontend/src/dispatchInput.test.ts
+++ frontend/src/dispatchInput.test.ts
@@ -1,13 +1,60 @@
-import { expect, test, describe, vi } from 'vitest'
+import { expect, it, describe, vi, afterEach, assert } from 'vitest'
 import { dispatchInput } from './dispatchInput'
+import {
+    Post,
+} from '../wailsjs/go/main/App.js'
+import {
+    BrowserOpenURL,
+} from '../wailsjs/runtime/runtime.js'
 
-describe.each([
-    { input: '', expected: '' },
-    { input: 'hello, world', expected: '' },
-    { input: '', expected: '' },
-])('dispatchInput($input) returns $expected', ({ input, expected }) => {
-    test(`returns ${expected}`, () => {
-        expect(dispatchInput(input)).toBe(expected)
+
+vi.mock('../wailsjs/go/main/App.js', () => ({
+    Post: vi.fn(),
+}))
+vi.mock('../wailsjs/runtime/runtime.js', () => ({
+    BrowserOpenURL: vi.fn(),
+}))
+
+describe('dispatchInput', () => {
+    afterEach(() => {
+        vi.resetAllMocks();
+    });
+
+    it('Post if input is usual messages', () => {
+        const msg = 'Hello, World'
+        dispatchInput(msg)
+        expect(Post).toHaveBeenCalledWith(msg)
+    })
+
+    it.each([
+        { input: "/help", helpUrl: 'https://github.com/kakakaya/mazesoba-continent/blob/main/README.md' },
+        { input: "/help command", helpUrl: 'https://github.com/kakakaya/mazesoba-continent/blob/main/docs/SLASH_COMMAND.md' },
+        { input: "/help config", helpUrl: 'https://github.com/kakakaya/mazesoba-continent/blob/main/docs/CONFIG.md' },
+    ])(`Open help page if input=$input`, ({ input, helpUrl }) => {
+        let resOk = '', resFail = ''
+        dispatchInput(input).then((res) => { resOk = res }).catch((res) => { resFail = 'fail'})
+        assert.equal(resOk, '')
+        assert.equal(resFail, '')
+        expect(BrowserOpenURL).toHaveBeenCalledWith(helpUrl)
+        expect(Post).not.toHaveBeenCalled()
+    })
+
+    it('Show error message if unknown help topic', () => {
+        const input = '/help unknown-topic'
+        let resOk = '', resFail = ''
+        dispatchInput(input).then((res) => { resOk = res }).catch((res) => { resFail = 'fail' })
+        expect(BrowserOpenURL).not.toHaveBeenCalled()
+        expect(Post).not.toHaveBeenCalled()
+
+        assert.equal(resOk, '')
+        assert.equal(resFail, '')
+    })
+
+    it.each([
+        // { input: "/open search foo", expected: "https://bsky.app/search?q=foo" },
+        // { input: " /open   search　まぜそば大陸　", expected: "https://bsky.app/search?q=まぜそば大陸" },
+    ])(`Open search page if input=$input`, ({ input, expected }) => {
+        expect(BrowserOpenURL).toHaveBeenCalledWith(expected)
     })
 })
 
--- frontend/src/dispatchInput.ts
+++ frontend/src/dispatchInput.ts
@@ -3,6 +3,50 @@ import {
     Post,
 } from '../wailsjs/go/main/App.js'
 
-export function dispatchInput(input: string): Promise<string> {
-    return new Promise(() => {return ""})
+import {
+    BrowserOpenURL,
+} from '../wailsjs/runtime/runtime.js'
+
+export function dispatchInput(input: string, dryRun: boolean = false): Promise<string> {
+    if (input.startsWith("/")) {
+        input = input.trim()
+        const args = input.split(" ");
+        switch (input) {
+            case "/help":
+                const helpArgs = args.slice(1);
+                if (dryRun) {
+                    return Promise.resolve(`Open help for ${helpArgs.join(" ")}`);
+                } else {
+                    return helpCommand(...helpArgs)
+                }
+                break;
+            default:
+                break;
+        }
+        return Promise.reject("Unknown command")
+    }
+    return Post(input)
+}
+
+export function helpCommand(...topics: string[]): Promise<string> {
+    const README = 'https://github.com/kakakaya/mazesoba-continent/blob/main/README.md'
+    const COMMAND = 'https://github.com/kakakaya/mazesoba-continent/blob/main/docs/SLASH_COMMAND.md'
+    const CONFIG = 'https://github.com/kakakaya/mazesoba-continent/blob/main/docs/CONFIG.md'
+    if (topics.length == 0) {
+        BrowserOpenURL(README)
+        return Promise.resolve("Opened?")
+    }
+    console.log(topics)
+    switch (topics.at(0)) {
+        case 'command':
+            BrowserOpenURL(COMMAND)
+            return Promise.resolve("Opened?")
+            break;
+        case 'config':
+            BrowserOpenURL(CONFIG)
+            return Promise.resolve("Opened?")
+        default:
+            break;
+    }
+    return Promise.reject("Unknown help page")
 }
