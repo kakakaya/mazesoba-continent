79ac0b74046268862855885647b7ac0d5c66c3b7
kakakaya <kakakaya+git@gmail.com>
2024-04-14 21:36:50 +0900

    Refactor CharCounter component to use max_count instead of MAX_COUNT

--- frontend/src/components/CharCounter.svelte
+++ frontend/src/components/CharCounter.svelte
@@ -1,18 +1,13 @@
 <script lang="ts">
     export let count = 0;
-    export const MAX_COUNT = 300; // this might be changed later
+    export let max_count = 300;  // default value is for Bluesky
     export let hidden: boolean = false;
-
-    function update(event: any) {
-        count = event.target.value.length;
-    }
 </script>
 
 <div
     class="char-counter"
-    class:char-counter--warning={count > MAX_COUNT}
+    class:char-counter--warning={count > max_count}
     {hidden}
-    on:change={update}
 >
     {count}
 </div>
@@ -20,9 +15,13 @@
 <style>
     .char-counter {
         color: #333;
+        width: min-content;
     }
     .char-counter--warning {
         background-color: #ffcccc;
         color: #990000;
+    }
+    .char-counter[hidden] {
+        display: none;
     }
 </style>
