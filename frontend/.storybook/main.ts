dc11577ab901d5d97559d7af09e0b096cc929a73
kakakaya <kakakaya+git@gmail.com>
2024-04-14 12:57:22 +0900

    Bump storybook

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -7,6 +7,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm"
   ],
   framework: {
     name: "@storybook/svelte-vite",
