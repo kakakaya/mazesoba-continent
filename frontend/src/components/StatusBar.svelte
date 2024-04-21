97ffec21c93f3e88dd4a68bc7e4a79bc99eab48d
kakakaya <kakakaya+git@gmail.com>
2024-04-21 22:01:39 +0900

    Update StatusBar component in StatusBar.stories.ts and StatusBar.svelte

---
+++ frontend/src/components/StatusBar.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import CharCounter from "./CharCounter.svelte";
+
+    export let helpMessage: string = "";
+    export let postFooter: string = "";
+    export let charCount: number = 0;
+    export let footer: string = "";
+    export let maxCount: number;
+</script>
+
+<div class="status-bar">
+    <div class="message status-element">{helpMessage}</div>
+    <div class="footer">{postFooter}</div>
+    <CharCounter class='status-element' count={charCount} max_count={maxCount} />
+</div>
+
+<style>
+    .status-bar {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        width: 100%;
+        display: flex;
+        color: white;
+        margin: 0px;
+        justify-content: flex-end;
+        border-top: 1px solid rgba(255, 255, 255, 0.1);
+        flex-wrap: wrap;
+        justify-content: flex-end;
+        gap: 0em 1em;
+    }
+
+    .status-bar > div {
+        padding: 0.2em;
+    }
+</style>
