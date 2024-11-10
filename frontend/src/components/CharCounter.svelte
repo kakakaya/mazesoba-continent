35ceb7377d275ac3aff4249b0d1c5bb94e5cfbe0
kakakaya <kakakaya+git@gmail.com>
2024-11-10 13:18:09 +0900

    WIP

--- frontend/src/components/CharCounter.svelte
+++ frontend/src/components/CharCounter.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
     export let count = 0;
     export let maxCount = 300; // default value is for Bluesky
-    export let hidden: boolean = false;
 </script>
 
 <div
     class="char-counter"
     class:char-counter--warning={count > maxCount}
-    hidden={count < 0}
+    class:no-count={count < 0}
 >
     {count}
 </div>
@@ -21,7 +20,7 @@
         background-color: #ffcccc;
         color: #990000;
     }
-    .char-counter[hidden] {
+    .no-count {
         display: none;
     }
 </style>
